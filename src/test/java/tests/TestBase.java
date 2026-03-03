8bc5e95a5b3ed533ad3d81a5269953ee659348c7
Esina Svetlana <montesum1@gmail.com>
2026-03-03 19:46:02 +0300

    deleted unused imports deleted dependency javafaker

--- src/test/java/tests/TestBase.java
+++ src/test/java/tests/TestBase.java
@@ -3,7 +3,6 @@ package tests;
 import com.codeborne.selenide.Configuration;
 import com.codeborne.selenide.logevents.SelenideLogger;
 import helpers.Attach;
-import io.qameta.allure.Story;
 import io.qameta.allure.selenide.AllureSelenide;
 import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeAll;
